87ea08d4bc985fd1f1b324c3d185b752212b6fa5
SupunOshadha <supunoshadha23@gmail.com>
2025-03-01 17:31:37 +0530

    setting cors for deployment-5

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,25 @@ const router = require("./routes/index");
 const app = express();
 app.use(
   cors({
-    origin: ["https://recipe-share-project.vercel.app"],    // origin: process.env.FRONTEND_URL,
+    origin: "https://recipe-share-project.vercel.app",    // origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.options("*", cors());
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+// Debugging Middleware (Check CORS Headers)
+app.use((req, res, next) => {
+  console.log("Request Method:", req.method);
+  console.log("Request Headers:", req.headers);
+  next();
+});
 
 app.use("/api", router);
 // testing comment
